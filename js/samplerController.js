bec098069870593cdb02fd36a4f00294bb09de18
L3X <64853354+L3X-TLH@users.noreply.github.com>
2024-05-08 11:17:33 +0200

    Update and rename songLoader.js to samplerController.js
    Play song multiple times, better SampleController, fixed toStart, toEnd

---
+++ js/samplerController.js
@@ -0,0 +1,43 @@
+const debugInstrumentsFolders = "../prova_tone_js/tonejs-instruments-master/tonejs-instruments-master/samples/"
+const debugAvailableSounds = [
+	"C1", "C#1", "D1", "D#1", "E1", "F1", "F#1", "G1", "G#1", "A1", "A#1", "B1",
+	"C2", "C#2", "D2", "D#2", "E2", "F2", "F#2", "G2", "G#2", "A2", "A#2", "B2",
+	"C3", "C#3", "D3", "D#3", "E3", "F3", "F#3", "G3", "G#3", "A3", "A#3", "B3",
+	"C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4",
+	"C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5", "G#5", "A5", "A#5", "B5",
+	"C6", "C#6", "D6", "D#6", "E6", "F6", "F#6", "G6", "G#6", "A6", "A#6", "B6",
+	"C7", "C#7", "D7", "D#7", "E7", "F7", "F#7", "G7", "G#7", "A7", "A#7", "B7"
+]
+
+export var SamplerController = {
+	loadSampler: function (samplesFolderUrl, availableSounds) {
+		let sounds = {}
+		debugAvailableSounds.forEach(sound => {
+			if (sound.includes("#")) {
+				let values = sound.split("#")
+				sounds[sound] = values[0] + "s" + values[1] + ".mp3"
+			} else {
+				sounds[sound] = sound + ".mp3"
+			}
+		});
+
+		const sampler = new Tone.Sampler({
+			urls: sounds,
+			baseUrl: debugInstrumentsFolders + samplesFolderUrl + "/"
+		}).toDestination()
+
+		return sampler
+	},
+
+	playSong: function (songJson, samplers, playheadPosition) {
+		for (const trackId in songJson.tracks) {
+			let sampler = samplers[trackId]
+			let notes = songJson.tracks[trackId].notes
+
+			for (const notePos in notes) {
+				let noteData = notes[notePos].split(",")
+				sampler.triggerAttackRelease(noteData[0], noteData[1], ("+" + noteData[2]))
+			}
+		}
+	}
+}
